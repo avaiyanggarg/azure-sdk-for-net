d9d4d04c94cbb38db4ac70e20504adb6725e60a9
Christopher Scott <chriss@microsoft.com>
2022-12-06 12:38:29 -0600

    fix

--- sdk/identity/test-resources-post.ps1
+++ sdk/identity/test-resources-post.ps1
@@ -7,7 +7,7 @@ param (
 
 Write-Host $DeploymentOutputs['IDENTITY_WEBAPP_USER_DEFINED_IDENTITY']
 Write-Host $DeploymentOutputs['IDENTITY_STORAGE_NAME_2']
-md -Force ./artifacts/bin/Integration.Identity.WebApp/Pub
-dotnet publish ./sdk/identity/Azure.Identity/integration/WebApp/Integration.Identity.WebApp.csproj -o ./artifacts/bin/Integration.Identity.WebApp/Pub
-Compress-Archive -Path ./artifacts/bin/Integration.Identity.WebApp/Pub/* -DestinationPath ./artifacts/bin/Integration.Identity.WebApp/Pub/package.zip  -Force
-az webapp deploy --resource-group $DeploymentOutputs['IDENTITY_RESOURCE_GROUP'] --name $DeploymentOutputs['IDENTITY_WEBAPP_NAME'] --src-path ./artifacts/bin/Integration.Identity.WebApp/Pub/package.zip
+dotnet publish ./sdk/identity/Azure.Identity/integration/WebApp/Integration.Identity.WebApp.csproj -o ./sdk/identity/Azure.Identity/integration/WebApp/Pub
+Compress-Archive -Path ./sdk/identity/Azure.Identity/integration/WebApp/Pub/* -DestinationPath ./sdk/identity/Azure.Identity/integration/WebApp/Pub/package.zip  -Force
+az webapp deploy --resource-group $DeploymentOutputs['IDENTITY_RESOURCE_GROUP'] --name $DeploymentOutputs['IDENTITY_WEBAPP_NAME'] --src-path ./sdk/identity/Azure.Identity/integration/WebApp/Pub/package.zip
+rd -Force -Recurse ./sdk/identity/Azure.Identity/integration/WebApp/Pub
